c99e8150d40b9318dc6c3f56ee35bf007b000b18
unknown <luisfelipesdn12@gmail.com>
2020-10-20 15:12:18 -0300

    Add an C implementation

---
+++ primo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define false 0
+#define true 1
+
+int main() {
+    int N = 0;
+    int* primos = malloc(N * sizeof(int));
+
+    for(int n = 2; true; n++) {
+        int add = true;
+
+        for (int i = 0; i < N; i++) {
+            if(n % primos[i] == 0) {
+                add = false;
+                break;
+            }
+        }
+        
+        if(add) {
+            N++;
+            primos = realloc(primos, N * sizeof(int));
+            primos[N-1] = n;
+            printf("%d\n", n);
+        }
+    }
+}
